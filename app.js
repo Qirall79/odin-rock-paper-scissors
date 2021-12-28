90e636bf2e65fa18967874d50b29a3b505cc28e1
qirall79 <belfatmi79@gmail.com>
2021-12-28 17:47:39 +0100

    Add UI to the game

--- app.js
+++ app.js
@@ -25,44 +25,62 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// Play 5 rounds and print the winner
-function game(){
-    //initializing the scores
-    let playerScore = 0;
-    let computerScore = 0;
+//initializing the scores
+let playerScore = 0;
+let computerScore = 0;
 
-    //continue until 5 rounds without tie are played and increase the score of the winner
-    while(playerScore + computerScore < 5){
-        //initializing choices
-        const computerSelection = computerPlay();
-        let playerSelection = prompt("Your choice: ");
 
-        //checking if the value entered by the user is valid
-        if(playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !== "paper" && playerSelection.toLowerCase() !== "scissors"){
-            alert("Choose a valid choice !");
-            playerSelection = prompt("Your choice: ");
+//Adding click event to each of the images
+const select = document.querySelectorAll(".select img");
+
+select.forEach(selection => {
+    selection.addEventListener("click", e => {
+
+        //Hiding any result paragraph from the previous round
+        document.querySelector(".lost").style.display = "none";
+        document.querySelector(".won").style.display = "none";
+        document.querySelector(".tie").style.display = "none";
+
+        //Taking the player and computer choices and storing the results in the "winner" variable
+        const playerChoice = e.target.getAttribute("alt");
+        const computerChoice = computerPlay();
+        const winner = playRound(playerChoice, computerChoice);
+
+        //Update score and display result paragraph below
+        if(winner === "computer"){
+            computerScore += 1;
+            document.querySelector(".lost").textContent = `You Lost! The computer chose ${computerChoice}`;
+            document.querySelector(".lost").style.display = "inline-block";
         }
-
-
-        //playing 1 round and storing its result in the result variable
-        const result = playRound(playerSelection, computerSelection);
-
-        if(result == "player"){
-            playerScore++;
-            console.log("You won, computer chose " + computerSelection);
-        }
-
-        else if(result == "computer"){
-            computerScore++;
-            console.log("You lost, computer chose " + computerSelection)
+        else if(winner == "player"){
+            playerScore += 1;
+            document.querySelector(".won").textContent = `You Won! The computer chose ${computerChoice}`;
+            document.querySelector(".won").style.display = "inline-block";
         }
         else{
-            console.log("Tie!")
+            document.querySelector(".tie").style.display = "inline-block";
         }
-        
-    }
 
-    //print results in the console
-    return playerScore > computerScore ? console.log("You Won the game !") : console.log("You Lost ! Try next time.");
+        document.querySelector(".computer").textContent = computerScore;
+        document.querySelector(".player").textContent = playerScore;
 
-}
+
+        //Announcing the winner (the one who reached 5 rounds win)
+        if(playerScore == 5){
+            document.querySelector(".finish p").textContent = "Ma3ndi mantsalk rak naaaadi(a)";
+            document.querySelector(".finish").style.display = "flex"
+        }
+
+        else if(computerScore == 5){
+            document.querySelector(".finish p").textContent = "Yawdi yawdi khassr(a) m3a algorithme";
+            document.querySelector(".finish").style.display = "flex"
+        }
+    })
+})
+
+
+//refresh the page when the "Plaey Again" button is clicked
+document.querySelector("button").addEventListener("click", e => {
+    document.location.reload(true);
+})
+
